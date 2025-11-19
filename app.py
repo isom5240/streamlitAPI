f0dab4ed8368754e97098b780f7999490ef2a48d
isom5240 <isom5240@ggmail.ust.hk>
2025-11-19 13:44:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,11 @@ from PIL import Image
 st.title("Image-to-Text and Text-to-Speech App")
 
 image_to_text = pipeline(
-    "image-to-text",
-    model="nlpconnect/vit-gpt2-image-captioning")
+  "image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
 text_to_speech = pipeline(
-    "text-to-speech",
-    model="facebook/mms-tts-eng")
+  "text-to-speech", model="facebook/mms-tts-eng")
 
-uploaded_file = st.file_uploader("Upload an image", 
-                                 type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 if uploaded_file:
     image = Image.open(uploaded_file)
     st.image(image)
@@ -25,6 +22,6 @@ if uploaded_file:
     audio = text_to_speech(caption)
     audio_path = "speech.wav"
     with open(audio_path, "wb") as f:
-        f.write(audio["audio"])
+      f.write(audio["audio"])
 
     st.audio(audio_path)
