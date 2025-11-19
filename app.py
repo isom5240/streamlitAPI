b19e98ea37f2a7eaef7e4830c2a324fd7a7a759d
isom5240 <isom5240@ggmail.ust.hk>
2025-11-19 19:44:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from transformers import pipeline
 def main():
     sentiment_pipeline = pipeline(model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
 
-    st.title("Sentiment Analysis with HuggingFace Spaces")
+    st.title("Sentiment Analysis with HuggingFace Spaces (ISOM5240)")
     st.write("Enter a sentence to analyze its sentiment:")
 
     user_input = st.text_input("")
